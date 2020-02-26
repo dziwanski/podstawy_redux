921061d186e40c38da9eea4adecaf6e316b84406
Andrzej Iwanski <iwanski@fev.com>
2020-02-26 18:03:01 +0100

    Create form.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import MoviesContainer from "./app/movies/components/MoviesContainer";
+import MoviesForm from "./app/movies/components/MoviesForm";
 
 function App() {
     return (
         <div className="App">
             <MoviesContainer/>
+            <MoviesForm/>
         </div>
     );
 }
