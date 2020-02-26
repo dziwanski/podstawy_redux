58fce3507577f0409d59629233fcb1683d06224a
Andrzej Iwanski <iwanski@fev.com>
2020-02-26 16:16:54 +0100

    Combined reducer.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {createStore, combineReducers, bindActionCreators} from "redux";
+import {createStore} from "redux";
 import {composeWithDevTools} from 'redux-devtools-extension';
+import rootReducer from './reducers';
+import {movieActions} from "./app/movies/duck";
 
 
-
-
-
-
-const allReducers = combineReducers({movies, actors});
-
-const store = createStore(allReducers, composeWithDevTools());
+const store = createStore(rootReducer, composeWithDevTools());
 window.store = store;
 
-// Creators for movies
-
-const movieActions = bindActionCreators({add: addMovie, reset: resetMovie}, store.dispatch);
-
-// Creators for actors
-
-const actorActions = bindActionCreators({add: addActor, reset: resetActors}, store.dispatch);
-
-store.dispatch(addActor('Cezary pazura'));
-
-movieActions.reset();
-movieActions.add('Chlopaki z barakow');
-
-actorActions.reset();
-actorActions.add('Ja');
-
+store.dispatch(movieActions.add('Rambo V'));
 
 function App() {
     return (
